a2b7dd0a81103149eb60e4ad89507e16e8a9beec
Kacper <wysockikacper98@gmail.com>
2021-01-21 17:49:27 +0100

    Fight with Cross Origin XD
    Took 4 minutes

--- src/main/java/app/bank/controller/AccountController.java
+++ src/main/java/app/bank/controller/AccountController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/bank")
 public class AccountController {
